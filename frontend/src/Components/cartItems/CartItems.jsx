8d41dd2d0972506e7dea8ac5dae0626c79dfe360
Esther Toyin <amooesther2503@gmail.com>
2024-04-26 12:50:38 +0100

    Adjustment

--- frontend/src/Components/cartItems/CartItems.jsx
+++ frontend/src/Components/cartItems/CartItems.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './CartItems.css'
 import trash from '../../Assets/trash.png'
 import close from '../../Assets/close.png'
+import fishTwo from '../../Assets/fishTwo.png'
+import heroImgTwo from '../../Assets/subTwo.png'
 
 
 const CartItems = () => {
@@ -9,7 +11,7 @@ const CartItems = () => {
     <div >
       <div className='cartItemsWrapper'>
       <div className='cartIt'>
-        <div ><img src="https://s3-alpha-sig.figma.com/img/8d46/4db1/65f215516db89a1ea4c585027a98295f?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kl7XpMS~~c1Dnl-eYkfOTCzqYhMrU2YxVcRPXLy9NdKbUT2IIrn0SkTpKurqbhaZi5UO7CxmQRPoWiKu9mB~jEVJ5mSn2aiYtELilztQUaexmgHw1MywodQuzg~hk1TOrPT8bjFYQlCdZGPYtQyp-2jWRZv6~fsxuy26aXyfUw7H843hznf5Z8-Zk-Y8cOJDAziIeqKvde3sLqcvNhLLTY2i~5UIl3UFY1dknLx~-BmouNQrPRTjBH6wDbMQ8ihl20UFLHNTLznCQExx~AI8Tsa54TpourN1sg6nrfogG5Nmkryzc~N3kTiKn37vJQF-Ljl~a8cTJtyBX3DzFSpJtQ__" alt="" /></div>
+        <div ><img src={fishTwo} /></div>
       <div className='cartItText'>
         <p>Fiesta whole</p>
         <span>N60,000</span>
@@ -29,7 +31,7 @@ const CartItems = () => {
       </div>
       <div className='cartItemsWrapper'>
       <div className='cartIt'>
-        <div ><img src="https://s3-alpha-sig.figma.com/img/4ecb/7bf8/a2f77bd5dbc6ce9fd4efe18ece0eb3ea?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TDEB4tMa~WPEKyFEFNHSWVg7BqvqR2l4RXZYNm2JCJ~1F2a2EmuFmVxBImzOGgr0Ro-jrLyeZHQzrS1CoidqP86OB3bKC8QuBV00V7FKYXeQ~DcHpIcbc5d4cWXGZ-qYCVVvG2GK-W9akbbzLc-DgDDVadXcdfs7jekuxTwdhJAZrjZHIZGTkiQl7TYKh0xhAbZtQTISVL8sbjLZqKvU5nefMrkko~5zkwrP7fV87Hzjm7J-pIo7PdAO06gMwVwq6S2ykymdJ2WrlbB4R7xogwG2TQMj~JjT-ic1ODd6tGUXQe0kTOnqlpOKXSw5PAD42x153qQRfV21LUca0ev4hw__" alt="" /></div>
+        <div ><img src={heroImgTwo} alt="" /></div>
       <div className='cartItText'>
         <p>Chi Whole Chicken</p>
         <span>N60,000</span>
